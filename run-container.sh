9ef40806856a0bd492cc3d7497e2fba9c9e7f974
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-06-23 18:31:33 +0300

    fix script

--- run-container.sh
+++ run-container.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+set -e
+
+# Script for starting the container on production server
+
+docker build -t odysseus-geoserver .
+
 docker run \
   --name odysseus-gs \
-  # This is the network created in odysseus-backend docker-compose.yml
   --network odysseus-backend_odysseus-net \
   --restart unless-stopped \
   --health-cmd 'curl -sS http://127.0.0.1:8080/geoserver/web/ || exit 1' \
@@ -9,8 +15,7 @@ docker run \
   --health-retries 4 \
   --health-timeout 10s \
   -p 127.0.0.1:8090:8080 \
-  # Use if you want to replace the default datastore with one with correct db host and credentials
-  # -v ${PWD}/datastore.xml:/opt/geoserver/data_dir/workspaces/odysseus/odysseus/datastore.xml \
+  -v ${PWD}/datastore.xml:/opt/geoserver/data_dir/workspaces/odysseus/odysseus/datastore.xml \
   -d \
   odysseus-geoserver:latest
 
